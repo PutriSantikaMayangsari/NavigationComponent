1e8cce6b5756ec5c27024ee2bf9830e566da42d6
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-04-12 05:56:51 +0700

    Create ItemFragment.java

---
+++ ItemFragment.java
@@ -0,0 +1,47 @@
+package com.example.navigationcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class ItemFragment extends Fragment {
+
+
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.itemButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_itemFragment_to_bayarFragment);
+
+            }
+        });
+    }
+}
