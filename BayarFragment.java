4bbe389e4c918ef7b1529d64bd7a95cb3003a85c
PutriSantikaMayangsari <83493403+PutriSantikaMayangsari@users.noreply.github.com>
2022-04-12 05:56:22 +0700

    Create BayarFragment.java

---
+++ BayarFragment.java
@@ -0,0 +1,63 @@
+package com.example.navigationcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class BayarFragment extends Fragment {
+
+    public BayarFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bayar, container, false);
+    }
+
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        final NavController navController = Navigation.findNavController(view);
+//
+//        Button button = view.findViewById(R.id.bayarButton);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                navController.navigate(R.id.action_endgameFragment_to_startFragment);
+//                NavOptions navOptions = new NavOptions.Builder().setPopUpTo(R.id.itemFragment, true).build();
+//
+//                navController.navigate(R.id.action_keranjangFragment3_to_itemFragment, null, navOptions);
+//            }
+//        });
+//    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        Button button = view.findViewById(R.id.bayarButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_bayarFragment_to_keranjangFragment3);
+            }
+        });
+    }
+}
